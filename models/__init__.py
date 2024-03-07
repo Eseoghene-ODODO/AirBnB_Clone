a63fcd158a4b1f5387fc175c08577408656c5396
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-07 22:27:25 +0100

    airbnb clone

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,7 @@ creating a unique FileStorage instance for my project
 
 from .base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
 
 
 storage = FileStorage()
