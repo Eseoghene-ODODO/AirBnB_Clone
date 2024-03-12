8af213df043b7d0b04549b1ab0390c1afeedccb1
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-12 03:14:05 +0000

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,72 +1,61 @@
 #!/usr/bin/python3
 """
-A FileStorage class the serializes/desserializes instances to JSON file
+class FileStorage that serializes instances to a JSON file and
+deserializes JSON file to instances
 """
 
 from models.base_model import BaseModel
 import json
-from datetime import datetime
-from models.user import User
+from os.path import exists
 from models.place import Place
 from models.state import State
 from models.city import City
-from models.review import Review
 from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
-    """class body"""
-    __file_path = 'file.json'
+    """FileStorage class body"""
+    __file_path = "file.json"
     __objects = {}
-    CLASSES = {
-            'BaseModel': BaseModel,
-            'User': User,
-            'Place': Place,
-            'State': State,
-            'City': City,
-            'Amenity': Amenity,
-            'Review': Review
-            }
 
     def all(self):
-        """Returns the dictionary of the private class attribute __objects"""
-        return self.__class__.__objects
+        """Return the dictionary __objects."""
+        return self.__objects
 
     def new(self, obj):
-        """method(new) body"""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__class__.__objects[key] = obj
+        """Set in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to json"""
-        serialized_objs = {}
-        for key, obj in self.__class__.__objects.items():
-            serialized_objs[key] = obj.__dict__
-        with open(self.__class__.__file_path, "w", encoding='utf-8') as file:
-            json.dump(serialized_objs, file, default=self.serialize_datetime)
-
-    @staticmethod
-    def serialize_datetime(obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        return obj
+        """Serialize __objects to the JSON file (path: __file_path)."""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
-        """desrializes json to objects"""
-        try:
-            with open(
-                    self.__class__.__file_path, "r", encoding='utf-8'
-                    ) as file:
-                data = json.load(file)  # data is handling deserialization
-                for key, value in data.items():
+        """Deserialize the JSON file to __objects."""
+        if exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, obj_dict in data.items():
                     class_name, obj_id = key.split('.')
-                    obj_class = self.CLASSES[class_name]
-                    new_obj = obj_class(**value)
-                    self.__class__.__objects[key] = new_obj
-        except (FileNotFoundError, json.JSONDecodeError):
-            pass
-
-
-if __name__ == '__main__':
-    storage = FileStorage()
-    storage.reload()
+                    obj = None  # Initialize obj here
+                    if class_name == 'State':
+                        obj = State(**obj_dict)
+                    elif class_name == 'City':
+                        obj = City(**obj_dict)
+                    elif class_name == 'Amenity':
+                        obj = Amenity(**obj_dict)
+                    elif class_name == 'Place':
+                        obj = Place(**obj_dict)
+                    elif class_name == 'Review':
+                        obj = Review(**obj_dict)
+                    elif class_name == 'BaseModel':
+                        obj = BaseModel(**obj_dict)
+                    else:
+                        obj = BaseModel(**obj_dict)
+                    self.__objects[key] = obj
